9d36ab5d71d9a198f7c8df7617dc47a531af0484
Chirkov Slava <slavachirkov92@gmail.com>
2023-06-23 23:34:32 +0300

    edit accordeon

--- src/js/script.js
+++ src/js/script.js
@@ -15,80 +15,116 @@ closeMenu.addEventListener('click', () => {
 //accordeon
 
 const accounts = document.getElementById('accounts'),
-    accountsHead = document.querySelectorAll('.accounts__head'),
+    accountsHeader = document.querySelectorAll('.accounts__head'),
+    accountsHead = document.querySelectorAll('.accounts__head-pointer'),
     accountsItem = document.querySelectorAll('.accounts__head-arrow--link'),
     accountsList = document.querySelectorAll('.accounts__check-list');
 
+accounts.addEventListener('click', (e) => {
+    if (e.target.matches('.accounts__head-arrow--link')) {
+        const btn = e.target.closest('.accounts__head-arrow--link');
 
-accountsItem.forEach((item, i) => {
-    item.addEventListener('click', (e) => {
-        e.preventDefault()
+        accountsItem.forEach((item, i) => {
+            e.preventDefault()
 
-        if (!item.classList.contains('active')) {
-            accountsItem[i].classList.add('active')
-            accountsList[i].classList.add('accounts__check-list--show')
+            if (btn === item) {
+                if (!item.classList.contains('active-acc')) {
+                    accountsHeader[i].classList.toggle('active')
+                    accountsItem[i].classList.toggle('active-acc')
+                    accountsHead[i].classList.toggle('active')
+                    accountsList[i].classList.add('accounts__check-list--show')
 
-            const h = getComputedStyle(accountsList[i]).height
+                    const h = getComputedStyle(accountsList[i]).height
 
-            accountsList[i].style.height = '0'
-            getComputedStyle(accountsList[i]).height
-            accountsList[i].style.height = h
+                    accountsList[i].style.height = '0'
+                    getComputedStyle(accountsList[i]).height
+                    accountsList[i].style.height = h
 
-            setTimeout(() => {
-                accountsList[i].style.height = ''
-                accountsHead[i].style.overflow = 'visible'
-            }, 360);
-        } else {
-            accountsList[i].style.height = getComputedStyle(accountsList[i]).height
-            getComputedStyle(accountsList[i]).height
-            accountsList[i].style.height = ''
-            accountsHead[i].style.overflow = 'hidden'
-            accountsItem[i].classList.remove('active')
-            accountsList[i].classList.remove('accounts__check-list--show')
-        }
-    })
+                    setTimeout(() => {
+                        accountsList[i].style.height = ''
+                        accountsHead[i].style.overflow = 'visible'
+                    }, 360);
+                } else {
+                    accountsList[i].style.height = getComputedStyle(accountsList[i]).height
+                    getComputedStyle(accountsList[i]).height
+                    accountsList[i].style.height = ''
+                    accountsHead[i].style.overflow = 'hidden'
+                    accountsHeader[i].classList.toggle('active')
+                    accountsItem[i].classList.toggle('active-acc')
+                    accountsHead[i].classList.toggle('active')
+                    accountsList[i].classList.remove('accounts__check-list--show')
+                }
+            }
+        })
+    } else if (e.target.matches('.accounts__head-pointer')) {
+        const btn = e.target.closest('.accounts__head-pointer');
+
+        accountsHead.forEach((item, i) => {
+            e.preventDefault()
+
+            if (btn === item) {
+                if (!item.classList.contains('active')) {
+                    accountsHeader[i].classList.toggle('active')
+                    accountsHead[i].classList.toggle('active')
+                    accountsItem[i].classList.toggle('active-acc')
+                    accountsList[i].classList.add('accounts__check-list--show')
+
+                    const h = getComputedStyle(accountsList[i]).height
+
+                    accountsList[i].style.height = '0'
+                    getComputedStyle(accountsList[i]).height
+                    accountsList[i].style.height = h
+
+                    setTimeout(() => {
+                        accountsList[i].style.height = ''
+                        accountsHead[i].style.overflow = 'visible'
+                    }, 360);
+                } else {
+                    accountsList[i].style.height = getComputedStyle(accountsList[i]).height
+                    getComputedStyle(accountsList[i]).height
+                    accountsList[i].style.height = ''
+                    accountsHead[i].style.overflow = 'hidden'
+                    accountsHeader[i].classList.toggle('active')
+                    accountsHead[i].classList.toggle('active')
+                    accountsItem[i].classList.toggle('active-acc')
+                    accountsList[i].classList.remove('accounts__check-list--show')
+                }
+            }
+        })
+    } else if (e.target.matches('.accounts__head')) {
+        const btn = e.target.closest('.accounts__head');
+
+        accountsHeader.forEach((item, i) => {
+            e.preventDefault()
+
+            if (btn === item) {
+                if (!item.classList.contains('active')) {
+                    accountsHeader[i].classList.toggle('active')
+                    accountsHead[i].classList.toggle('active')
+                    accountsItem[i].classList.toggle('active-acc')
+                    accountsList[i].classList.add('accounts__check-list--show')
+
+                    const h = getComputedStyle(accountsList[i]).height
+
+                    accountsList[i].style.height = '0'
+                    getComputedStyle(accountsList[i]).height
+                    accountsList[i].style.height = h
+
+                    setTimeout(() => {
+                        accountsList[i].style.height = ''
+                        accountsHead[i].style.overflow = 'visible'
+                    }, 360);
+                } else {
+                    accountsList[i].style.height = getComputedStyle(accountsList[i]).height
+                    getComputedStyle(accountsList[i]).height
+                    accountsList[i].style.height = ''
+                    accountsHead[i].style.overflow = 'hidden'
+                    accountsHeader[i].classList.toggle('active')
+                    accountsHead[i].classList.toggle('active')
+                    accountsItem[i].classList.toggle('active-acc')
+                    accountsList[i].classList.remove('accounts__check-list--show')
+                }
+            }
+        })
+    }
 })
-
-//table
-
-// const tableBtn = document.querySelectorAll('.deposits__table-link > a'),
-//     tableRow = document.querySelectorAll('.deposits__table-body > tr');
-
-// tableBtn.forEach((btn, i) => {
-//     btn.addEventListener('mouseenter', (e) => {
-//         const tableTd = tableRow[i].querySelectorAll('td');
-
-//         tableTd.forEach((item) => {
-//             item.style.backgroundColor = '#FE7E47'
-//             item.style.color = '#FFFFFF'
-//         })
-//         // tableRow[i].style.backgroundColor = '#FE7E47'
-//         // tableRow[i].style.color = '#FFFFFF'
-//     })
-
-//     btn.addEventListener('mouseleave', (e) => {
-//         const tableTd = tableRow[i].querySelectorAll('td');
-
-//         tableTd.forEach((item) => {
-//             item.style.backgroundColor = ''
-//             item.style.color = ''
-//         })
-//         // tableRow[i].style.backgroundColor = ''
-//         // tableRow[i].style.color = ''
-//     })
-// })
-
-// const tableBtn = document.querySelectorAll('.table-link > a'),
-//     tableRow = document.querySelectorAll('.table-item');
-
-// tableBtn.forEach((btn, i) => {
-//     btn.addEventListener('mouseenter', (e) => {
-//         tableRow[i].style.backgroundColor = '#FE7E47'
-//         tableRow[i].style.color = '#FFFFFF'
-//     })
-
-//     btn.addEventListener('mouseleave', (e) => {
-//         tableRow[i].style.backgroundColor = ''
-//         tableRow[i].style.color = ''
-//     })
-// })
